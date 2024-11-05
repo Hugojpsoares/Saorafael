4ffdeeb5e42f8618d8786692ba4e1bfba0d5a618
Hugojpsoares <hugojpsoares@gmail.com>
2024-11-05 13:30:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.menu-mobile .burger');
-    const mobileMenu = document.querySelector('.menu-mobile ul');
-
-    burger.addEventListener('click', () => {
-        mobileMenu.classList.toggle('show');
-    });
-});
